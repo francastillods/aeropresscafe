bbf791fd6578dbfc13f76b8a0ea92cf0ba580110
fran.castillo <fran.castillo@tecnologiasplexus.onmicrosoft.com>
2022-04-03 10:56:07 +0200

    feat: HeaderCta responsive

--- src/components/HeaderCta/HeaderCta.js
+++ src/components/HeaderCta/HeaderCta.js
@@ -6,22 +6,19 @@ export const HeaderCta = ({srcSet, imgAlt, claim, cta, href, title}) => {
 
   return (
     <header className="header-cta">
-        <figure>
-            <ImgWebp 
-                srcSet={ srcSet }
-                imgAlt= { imgAlt }
-            />
-        </figure>
         <div className="container">
           <div className='header-cta_info'>
             <h1>{ claim }</h1>
             <a className='btn btn-primary' href={ href } title={ title } target="_self">{ cta }</a>
           </div>
-
         </div>
-        {/* <div className="header-cta_info">
 
-        </div> */}
+        <figure>
+            <ImgWebp 
+                srcSet={ srcSet }
+                imgAlt= { imgAlt }
+            />
+          </figure>
     </header>
   )
 }
